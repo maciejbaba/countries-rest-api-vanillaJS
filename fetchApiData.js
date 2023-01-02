bc50ccc621d260ccc54270cac13751491e5831c1
maciek367 <maciek2baba2@gmail.com>
2023-01-02 19:22:21 +0100

    change region name from america to americas in order to for it to be the same as in the API response, add code that, during the response handling, takes values from country onlywhich will be used in the project

--- fetchApiData.js
+++ fetchApiData.js
@@ -1,14 +1,29 @@
 const API_URL_ALL_COUNTRIES = 'https://restcountries.com/v3.1/all';
 
-let countries = [];
+let modifiedCountries = [];
 
 fetch(API_URL_ALL_COUNTRIES)
 .then(res => res.json())
-.then(res => {
-  res.forEach(country => {
-    countries.push(country);
-  })
+.then(countries => {
+  countries.forEach(country => {
+
+    const modifiedCountry = {
+      name: country.name.common,
+      capital: country.capital,
+      population: country.population,
+      region: country.region,
+      flagLink: country.flags.png,
+      nativeName: country.name.nativeName,
+      subregion: country.subregion,
+      topLevelDomain: country.tld,
+      currencies: country.currencies,
+      languages: country.languages,
+      borders: country.borders || "No borders",
+    }
+
+    modifiedCountries.push(modifiedCountry);
+  });
 })
 .catch(err => console.log(err));
 
-export default countries;
+export default modifiedCountries ;
