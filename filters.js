d15e79f4e91ea8b68c4748aa74e5a174be1f11ae
maciek367 <maciek2baba2@gmail.com>
2023-01-03 18:20:42 +0100

    fix filters issue, after selecting region and searching by name, filters was looking for specified name in whole world despite specifing region

--- filters.js
+++ filters.js
@@ -4,16 +4,23 @@ import renderCountries from "./renderCountries.js";
 const inputEl = document.getElementById("search");
 const regionEl = document.getElementById("region");
 
+let region = "All";
+
 inputEl.addEventListener("input", (e) => {
   const input = e.target.value;
   const filteredCountries = countries.filter((country) => {
-    return country.name.toLowerCase().includes(input.toLowerCase());
+    if(region === "All") {
+      return country.name.toLowerCase().includes(input.toLowerCase());
+    }
+    else {
+      return country.name.toLowerCase().includes(input.toLowerCase()) && country.region.toLowerCase().includes(region.toLowerCase());
+    }
   });
   renderCountries(filteredCountries);
 });
 
 regionEl.addEventListener("change", (e) => {
-  const region = e.target.value;
+  region = e.target.value;
 
   if (region === "All") {
     renderCountries(countries);
